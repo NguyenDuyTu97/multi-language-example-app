89fd18a37e56ca78e693912d7e4fc6ed5e71d437
tund <tund@gemvietnam.com>
2024-07-22 16:52:09 +0700

    refactor code

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import TestComponent from "./testComponent";
 
 export const About = () => {
   const { t } = useTranslation();
@@ -8,6 +9,21 @@ export const About = () => {
       <h1>{t("about")}</h1>
       <h3>{t("hello")}</h3>
       <span>{t("user", { name: "Bruce Wayne 🦇" })}</span>
+
+      <TestComponent
+        name={{
+          VisualizationSidebarBox: { title: t("hello") },
+          // RegionLabel: {
+          //   isRequired: "This field is required",
+          //   classSelectBoxLabel: "Field",
+          //   classSelectBoxPlaceholder: "Field",
+          //   tagsSelectBoxLabel: "Tags",
+          //   tagsSelectBoxPlaceholder: "Tags",
+          //   commentBoxLabel: "Value",
+          //   commentBoxPlaceholder: "Value",
+          // },
+        }}
+      />
     </main>
   );
 };
---
+++ src/pages/about/testComponent.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TestComponent({ name }) {
+  return <div>test: {name.VisualizationSidebarBox.title}</div>;
+}
